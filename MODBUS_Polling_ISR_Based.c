eea825c66e10320d2db1fbff7d6ecdebd0cfb466
Phillip Roos <55896656+philliproos01@users.noreply.github.com>
2024-11-18 14:14:48 -0500

    Changed Watchdog timer to 30s to prevent reset while writing multiple registers

--- MODBUS_Polling_ISR_Based.c
+++ MODBUS_Polling_ISR_Based.c
@@ -4,7 +4,7 @@
 
 // Configuration bits
 #pragma config OSC = IRCIO67, FCMEN = OFF, IESO = OFF, PWRT = ON, BOREN = BOACTIVE
-#pragma config BORV = 3, WDT = ON, WDTPS = 512, PBADEN = OFF, LPT1OSC = OFF
+#pragma config BORV = 3, WDT = ON, WDTPS = 32768, PBADEN = OFF, LPT1OSC = OFF
 #pragma config MCLRE = OFF, STVREN = ON, LVP = OFF, BBSIZ = 1024, XINST = OFF
 #pragma config DEBUG = OFF, CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF
 #pragma config CPB = OFF, CPD = OFF, WRT0 = OFF, WRT1 = OFF, WRT2 = OFF
@@ -19,15 +19,15 @@
 #define DRIVER PORTDbits.RD1
 
 // Variables
-uint8_t slave_address;
-uint8_t function_code;
+uint16_t slave_address;
+uint16_t function_code;
 uint16_t start_address;
 uint16_t quantity;
 uint16_t crc;
-uint8_t rx_buffer[11];
-uint8_t tx_buffer[37];
+volatile uint8_t rx_buffer[11];
+volatile uint8_t tx_buffer[45];
 bool coil1, coil2;
-uint16_t holding_reg[16];
+volatile uint16_t holding_reg[20];
 uint8_t i_global, j;
 uint8_t temp_byte;
 uint8_t data_length;
@@ -36,6 +36,7 @@ bool NewFrame;
 uint8_t TMR1Ticks;
 uint8_t Length;
 
+uint8_t initialSetup = 0;
 // Function prototypes
 void initialize(void);
 void process_modbus_request(void);
@@ -48,66 +49,66 @@ void calculate_crc(void);
 void send_modbus_response(void);
 void receive_modbus_frame(void);
 
-void __interrupt() ISR(void) {
-    if (PIR1bits.RCIF) {
 
-        TRISDbits.TRISD1 = 0;
-        receive_modbus_frame();
-        if (rx_buffer[0] == slave_address) {
-            process_modbus_request();
-        }
-        PIR1bits.RCIF = 0;
-    }
-}
+
 
 
 void main(void) {
+    
+    
     initialize();
     
+    
+    
     while (1) {
-        for (int counter2 = 0; counter2 <= 5; counter2++) {
-            for (int counter1 = 0; counter1 <= 30000; counter1++) {
-                // Your main program code here
-            }
+        if (holding_reg[4] == 0xFFFF) {
+            holding_reg[4] = 0xFFFF;
+            LATDbits.LATD4 = 1;
+            __delay_ms(500);  // Delay for 500 ms
         }
-        LED0 = !LED0;
+        
     }
+    
 }
 
 void initialize(void) {
     // Clear memory
-    for (int i = 0; i < 16; i++) {
+    /*for (int i = 0; i < 19; i++) {
         holding_reg[i] = 0;
-    }
-    
+    }*/
+    TRISDbits.TRISD4 = 0;
     // Oscillator configuration
     OSCCONbits.IRCF2 = 1;
     OSCCONbits.IRCF1 = 1;
     OSCCONbits.IRCF0 = 0;
-    
+
     // UART configuration
     TXSTAbits.BRGH = 1;
     SPBRG = 25; // For 9600 baud at 4MHz
     RCSTAbits.SPEN = 1;
     RCSTAbits.CREN = 1;
     TXSTAbits.TXEN = 1;
-    
+
     // Enable UART receive interrupt
     PIE1bits.RCIE = 1;
     INTCONbits.PEIE = 1;
     INTCONbits.GIE = 1;
-    
+
     // Initialize variables
     slave_address = 1;
     coil1 = false;
     coil2 = false;
     rx_count = 0;
-    holding_reg[0] = 65535;
-    LED0 = 0;
     
-    NewFrame = false;
+    LATDbits.LATD4 = 0;
+    __delay_ms(500);
+    LATDbits.LATD4 = 1;
+    __delay_ms(500);
+    
     TMR1Ticks = 0;
     Length = 0;
+    initialSetup = 1;
+    //holding_reg[3] = 65535;
 }
 
 void process_modbus_request(void) {
@@ -116,18 +117,18 @@ void process_modbus_request(void) {
     quantity = rx_buffer[5];
     
     switch (function_code) {
-        case 1:
+        /*case 1:
             read_coils();
-            break;
+            break;*/
         case 3:
             read_holding_registers();
             break;
-        case 5:
+        /*case 5:
             write_single_coil();
-            break;
-        case 6:
+            break;*/
+        /*case 6:
             write_single_register();
-            break;
+            break;*/
         case 16:
             write_multiple_registers();
             break;
@@ -138,7 +139,7 @@ void read_coils(void) {
     tx_buffer[0] = slave_address;
     tx_buffer[1] = function_code;
     tx_buffer[2] = 1;
-    tx_buffer[3] = coil1 | (coil2 << 1);
+    tx_buffer[3] =  coil1 + (coil2 * 2);
     data_length = 4;
     calculate_crc();
     tx_buffer[4] = crc & 0xFF;
@@ -149,15 +150,15 @@ void read_coils(void) {
 void read_holding_registers(void) {
     tx_buffer[0] = slave_address;
     tx_buffer[1] = function_code;
-    tx_buffer[2] = 32;
-    for (i_global = 0; i_global < 16; i_global++) {
+    tx_buffer[2] = 40;
+    for (i_global = 0; i_global < 19; i_global++) {
         tx_buffer[3 + i_global * 2] = holding_reg[i_global] >> 8;
-        tx_buffer[4 + i_global * 2] = holding_reg[i_global] & 0xFF;
+        tx_buffer[4 + i_global * 2] = holding_reg[i_global] & 255;
     }
-    data_length = 35;
+    data_length = 43;
     calculate_crc();
-    tx_buffer[35] = crc & 0xFF;
-    tx_buffer[36] = (crc >> 8) & 0xFF;
+    tx_buffer[43] = crc & 0xFF;
+    tx_buffer[44] = (crc >> 8) & 0xFF;
     send_modbus_response();
 }
 
@@ -180,15 +181,24 @@ void write_single_register(void) {
 }
 
 void write_multiple_registers(void) {
-    quantity = (rx_buffer[7] << 8) | rx_buffer[8];
-    for (i_global = 0; i_global < quantity; i_global++) {
-        if (rx_buffer[3] + i_global < 16) {
-            holding_reg[rx_buffer[3] + i_global] = (rx_buffer[7 + i_global * 2] << 8) | rx_buffer[8 + i_global * 2];
-        }
+    //quantity = rx_buffer[7] * 256 + rx_buffer[8];
+    switch (rx_buffer[3]) {
+        case 0x04:
+            holding_reg[4] = (rx_buffer[7] << 8) | rx_buffer[8];
+            break;
+        case 0x05:
+            holding_reg[5] = (rx_buffer[7] << 8) | rx_buffer[8];
+            break;
+        case 0x10:
+            holding_reg[16] = (rx_buffer[7] << 8) | rx_buffer[8];
+            break;
+        case 0x11:
+            holding_reg[17] = (rx_buffer[7] << 8) | rx_buffer[8];
+            break;
     }
     
-    tx_buffer[0] = slave_address;
-    tx_buffer[1] = function_code;
+    tx_buffer[0] = 0x01;
+    tx_buffer[1] = 0x10;
     tx_buffer[2] = rx_buffer[2];
     tx_buffer[3] = rx_buffer[3];
     tx_buffer[4] = rx_buffer[4];
@@ -198,7 +208,14 @@ void write_multiple_registers(void) {
     tx_buffer[6] = crc & 0xFF;
     tx_buffer[7] = (crc >> 8) & 0xFF;
     
-    send_modbus_response();
+    for (i_global = 0; i_global < 8; i_global++) {
+        DRIVER = 1;
+        while (!TXSTAbits.TRMT);
+        TXREG = tx_buffer[i_global];
+    }
+    __delay_ms(10);
+    TRISDbits.TRISD1 = 1;
+    DRIVER = 0;
 }
 
 void calculate_crc(void) {
@@ -216,9 +233,9 @@ void calculate_crc(void) {
 }
 
 void send_modbus_response(void) {
-    for (i_global = 0; i_global < 37; i_global++) {
+    for (i_global = 0; i_global < 45; i_global++) {
         DRIVER = 1;
-        while (!TXSTAbits.TRMT);
+        while (!PIR1bits.TXIF);
         TXREG = tx_buffer[i_global];
     }
     __delay_ms(10);
@@ -228,21 +245,40 @@ void send_modbus_response(void) {
 
 void receive_modbus_frame(void) {
     TRISDbits.TRISD1 = 0;
-    for (i_global = 0; i_global < 3; i_global++) {
-        DRIVER = 0;
-        while (!PIR1bits.RCIF);
-        rx_buffer[i_global] = RCREG;
-        DRIVER = 1;
-    }
+    i_global = 0;
     
-    start_address = (rx_buffer[2] << 8) | rx_buffer[3];
-    if (start_address == 3) {
-        for (; i_global < 5; i_global++) {
+        for (;i_global < 8; i_global++) {
             DRIVER = 0;
             while (!PIR1bits.RCIF);
+            //asm("nop");
             rx_buffer[i_global] = RCREG;
+          //  i_global++;
+            DRIVER = 1;
+        }
+    function_code = rx_buffer[1];
+    if (function_code == 0x10) { 
+        while (i_global < 11) {
+            DRIVER = 0;
+            while (!PIR1bits.RCIF);
+            //asm("nop");
+            rx_buffer[i_global] = RCREG;
+            i_global++;
             DRIVER = 1;
         }
     }
+    
+
     __delay_us(50);
+}
+
+void __interrupt() ISR(void) {
+    if (PIR1bits.RCIF) {
+
+        TRISDbits.TRISD1 = 0;
+        receive_modbus_frame();
+        if (rx_buffer[0] == slave_address) {
+            process_modbus_request();
+        }
+        PIR1bits.RCIF = 0;
+    }
 }
